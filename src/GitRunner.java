69fd7abd3eb4f30e16fd5924c3765f5490304a34
MoiSauce <moisesmares38@gmail.com>
2017-05-15 13:51:28 -0600

    blah

--- src/GitRunner.java
+++ src/GitRunner.java
@@ -3,7 +3,8 @@ public class GitRunner {
 
 	public static void main(String[] args) {
 		System.out.println("Initial Builf - new project, yeah!");
-
+		System.out.println("Fix Alex's life");
+		//work
 	}
 
 }
